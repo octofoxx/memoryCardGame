a33beeb5fb8d912c4eaba49c19ea8f768b9ebc8b
octofoxx <devilmaycry87x@gmail.com>
2023-09-03 17:57:08 -0700

    matching logic and disabling clicking of same card

--- script.js
+++ script.js
@@ -1,7 +1,54 @@
 const cards = document.querySelectorAll('.card');
 
+let flippedCard = false;
+
+let pauseCards =false;
+
+let firstCard, secondCard;
+
 function flipCard () {
-    this.classList.toggle ('flip');
+    if (pauseCards) return;
+    if (this ===firstCard) return;
+    this.classList.add ('flip');
+
+    if (!flippedCard) {
+        flippedCard =true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+
+    checkMatch();
+}
+
+function checkMatch () {
+    if (firstCard.dataset.name === secondCard.dataset.name) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetCards();
+}
+
+function unflipCards() {
+    pauseCards = true;
+    setTimeout(()=> {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetCards();
+    }, 1000);
+}
+
+function resetCards() {
+    [flippedCard, pauseCards] = [false,false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
